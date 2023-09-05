d66027e633e431c115a29e9d43f16bc4b5d0f810
Eugene <131405736+EvgenBilous@users.noreply.github.com>
2023-09-04 23:50:41 -0500

    start task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+console.log(createGallery(galleryItems));
+const gallery = document.querySelector(".gallery");
+const cardsMarkup = createGallery(galleryItems);
+
+const imageContainer = gallery.insertAdjacentElement("beforeend", cardsMarkup);
+
+function createGallery({ original, preview, description }) {
+  return galleryItems
+    .map((image) => {
+      return `<div>
+  <li><img src="${original}" alt=""></li>
+</div>`;
+    })
+    .join(` `);
+}
+galle;
+//console.log(galleryItems);
