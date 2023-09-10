e990a89cf7704c06d377c417eaf319c3a0909a8a
Eugene <131405736+EvgenBilous@users.noreply.github.com>
2023-09-10 01:30:57 -0500

    added style wip

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,20 +1,55 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(createGallery(galleryItems));
-const gallery = document.querySelector(".gallery");
-const cardsMarkup = createGallery(galleryItems);
+//import * as basicLightbox from "basiclightbox";
 
-const imageContainer = gallery.insertAdjacentElement("beforeend", cardsMarkup);
+// instance.show();
+const listImages = document.querySelector(".gallery");
 
-function createGallery({ original, preview, description }) {
-  return galleryItems
-    .map((image) => {
-      return `<div>
-  <li><img src="${original}" alt=""></li>
-</div>`;
+function createMarkup(arr) {
+  return arr
+    .map((image, link, description, original, preview) => {
+      //   return `
+
+      // <li class=".gallery__item"><img class = "gallery__image" src="${image.preview}" alt="${image.alt}" width="300" height="200"></li>
+      // `
+      `<li class="gallery__item">
+  <a class="gallery__link" href="${image.preview}">
+    <img
+      class="gallery__image"
+      src="${image.original}"
+      data-source="large-image.jpg"
+      alt="${image.alt}"
+    />
+  </a>
+</li>  `;
     })
-    .join(` `);
+    .join("");
 }
-galle;
-//console.log(galleryItems);
+const container = document.querySelector(".gallery");
+const markup = createMarkup(galleryItems);
+console.log(markup);
+listImages.insertAdjacentHTML("afterbegin", markup);
+listImages.style.cssText = " display: flexwrap; grid-gap:40px";
+
+container.addEventListener("click", handlePictureClick);
+
+function handlePictureClick(event) {
+  const targetElement = event.target.closest(".gallery__item");
+  // if (event.target === event.currentTarget) {
+  //   return;
+  // }
+  console.log("Event target");
+  console.log(event.target);
+  console.log("Our Target");
+  console.log(targetElement);
+}
+
+// const imageId = targetElement.dataset.id;
+// const imageInfo = galleryItems.find((image) => image.id === Number(imageId));
+// const instance = basic.Lightbox.create(`
+// // <div class="modal">
+// //      <img src="assets/images/image.png" width="800" height="600">
+
+// // </div>`);
+instance.show();
