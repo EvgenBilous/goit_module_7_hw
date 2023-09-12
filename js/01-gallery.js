fe3ab2173c9ba391021310d4436780c0177c4652
Eugene <131405736+EvgenBilous@users.noreply.github.com>
2023-09-11 23:26:05 -0500

    task2 started

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -36,7 +36,7 @@ function handleImageClick(event) {
   const imageItem = targetElement.dataset.source;
   console.log("this is original image: ", imageItem);
   const instance = basicLightbox.create(`
-    <div class="modal">
+    <div id="modal_window" class="modal">
  <img src="${imageItem}" alt="${descriptionItem}" />
 <h3>${descriptionItem}</h3>
       </div>
@@ -44,6 +44,35 @@ function handleImageClick(event) {
 
   instance.show();
 }
-// код який непрацює
-//<img src="${imageItem}" alt="${description}" />
-// <h3>${description}</h3>
+
+// if (event.keycode == 27) {
+//   // If ESC key is pressed
+//   if (document.querySelector("modal_window").classList.contains("modal")) {
+//     // LIGHTBOX ON
+//     document.querySelector("modal_window").classList.remove("modal");
+//   }
+// }
+// document.onkeydown = function (evt) {
+//   if (evt.keyCode == 27) {
+//     var modal = document.getElementById("modal");
+//     modal.style.display = "none";
+//     //modal_window.style.
+//     // Escape key pressed
+//     // Closes modal (adding class that sets it back to display: none) upon user's clicking its close icon
+//     // const listImages2 = document.querySelector(".gallery__item");
+//     // listImages2.addEventListener("click", function () {
+//     //   modal.classList.remove("basicLightbox--visible");
+//     // });
+//   }
+// };
+document.onkeydown = function (evt) {
+  var modal = document.getElementById("modal");
+
+  if (evt.keyCode == 27) {
+    //modal_window.style.basicLightbox.display = "none";
+    const listImages2 = document.querySelector(".gallery__item");
+    listImages2.addEventListener("click", function () {
+      modal.classList.remove("basicLightbox");
+    });
+  }
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const listImages = document.querySelector(".gallery");
+const markup = createMarkup(galleryItems);
+listImages.insertAdjacentHTML("beforeend", markup);
+listImages.addEventListener("click", handleImageClick);
+
+function createMarkup(arr) {
+  return arr
+    .map(({ original, preview, description }) => {
+      return `<div>
+<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+</div>
+ `;
+    })
+    .join("");
+}
+console.log(markup);
+
+//console.log(galleryItems);
